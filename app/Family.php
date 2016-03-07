afc098a9b4139bc541f4e34a989b19621cc8f047
Ivan Israel <ivanisrael.hernaez@gmail.com>
2016-03-07 08:51:07 +0800

    Created Models

---
+++ app/Family.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace brisgis;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Family extends Model
+{
+    //
+}
---
+++ app/Household.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace brisgis;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Household extends Model
+{
+    //
+}
