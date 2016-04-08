1800afe171f8eb09c1968088bd05dde2bafc5e37
CJ Santiago <CJ Santiago>
2016-04-08 16:52:34 +0800

    Household Page

--- resources/views/pages/households/index.blade.php
+++ resources/views/pages/households/index.blade.php
@@ -1,16 +1,105 @@
 @extends('layouts.app')
 
+@include('pages.households.add_modal')
+
 @section('main-content')
+  <section class="content-header">
+          <h1>
+             Barangay Hinaplanon 
+          </h1>      
+        </section>
+        <!-- Main content -->
+        <section class="content">
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="box">
+                <div class="box-header">
+                    <div class="col-xs-3">   
+                      <h3 class="box-title">List of Households</h3>
+                    </div>
+                    <div class="col-xs-0">
+                      <a data-toggle="modal" data-target="#add-household" class="btn btn-primary btn-sm pull-right">
+                          <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> 
+                          Add Household
+                      </a>
+                    </div>                  
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                  <table id="example1" class="table table-bordered table-striped">
+                     <thead>
+                       <tr>
+                          <th>ID</th>
+                          <th>Household Name</th>
+                          <th>usage</th>
+                          <th>Structure</th>
+                          <th>Area</th>
+                          <th>Purok Name</th>
+                          <th><center>Edit</center></th>
+                          <th><center>Delete</center></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td><a href="#">Hernaez Family</a></td>
+                        <td>Residential</td>
+                        <td>wood</td>
+                        <td>40</td>
+                        <td>1A</td>
+                        <td>
+                            <center>
+                              <a href="#" data-toggle="modal" data-target="#edit-household" >
+                                <span class="glyphicon glyphicon-edit text-info" aria-hidden="true"></span>
+                              </a>
+                            </center>
+                        </td>
+                        <td>
+                            <center>
+                              <a href="#" data-toggle="modal" data-target="#delete-household" >
+                                <span class="glyphicon glyphicon-remove text-danger" aria-hidden="true"></span>
+                              </a>
+                            </center>
+                        </td>
+                      </tr>                               
+                      @include('pages.households.delete_modal')
+                      @include('pages.households.edit_modal')
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                         <th>ID</th>
+                          <th>Household Name</th>
+                          <th>usage</th>
+                          <th>Structure</th>
+                          <th>Area</th>
+                          <th>Purok Name</th>
+                          <th><center>Edit</center></th>
+                          <th><center>Delete</center></th>
+                      </tr>
+                   </tfoot>
+                  </table>
+                </div><!-- /.box-body -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </section><!-- /.content -->
+@endsection
 
-<div class="row">
-    <div class="col-md-3">
-        <div class="panel panel-default">
-            <div class="panel-heading">Home</div>
-                <div class="panel-body">
-                  <div> Hello! </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
+@section('page-script')
+    <!-- DataTables -->
+    <script src="{{ asset('/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('/plugins/datatables/dataTables.bootstrap.min.js') }}"></script>
+    <!-- page script -->
+    <script>
+      $(function () {
+        $("#example1").DataTable();
+        $('#example2').DataTable({
+          "paging": true,
+          "lengthChange": false,
+          "searching": false,
+          "ordering": true,
+          "info": true,
+          "autoWidth": false
+        });
+      });
+    </script>
 @endsection
